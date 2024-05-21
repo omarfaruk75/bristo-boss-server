c897f091dd3db31d3f59dc560d022759f2d488a9
omar faruk <farukomarcpa@gmail.com>
2024-05-21 18:23:16 +0600

    delete api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT||5000;
 
@@ -26,6 +26,7 @@ async function run() {
     
     const menuCollection = client.db("bossDB").collection("menu");
     const reviewsCollection = client.db("bossDB").collection("reviews");
+    const cartsCollection = client.db("bossDB").collection("carts");
     //for menu
   app.get('/menu',async(req,res)=>{
     const result = await menuCollection.find().toArray()
@@ -38,6 +39,28 @@ async function run() {
   app.get('/reviews',async(req,res)=>{
     const result = await reviewsCollection.find().toArray()
     res.send(result)
+
+  })
+ app.get('/carts', async(req,res)=>{
+    const email = req.query.email;
+    const query = {email :email};
+    const result = await cartsCollection.find(query).toArray();
+    res.send(result)
+  })
+
+  //carts collection
+  app.post('/carts', async(req,res)=>{
+    const cartItem = req.body
+    const result = await cartsCollection.insertOne(cartItem);
+    res.send(result)
+  })
+
+  //delete cart item
+  app.delete('/carts/:id',async(req,res)=>{
+    const id = req.params.id
+    const query = {_id:new ObjectId(id)}
+    const result = await cartsCollection.deleteOne(query)
+    res.send(result) 
 
   })
     await client.db("admin").command({ ping: 1 });
@@ -57,3 +80,19 @@ app.get('/',(req,res)=>{
 app.listen(port,()=>{
     console.log(`bistro boss in running on port ${port}`);
 })
+
+
+
+
+
+/**
+ * --------------------------------------------------------
+ * Naming Convention
+ * --------------------------------------------------------
+ * app.get('/users)
+ * app.get('/usert/:id)
+ * app.post('/users)
+ * app.put('/users/:id)
+ * app.patch('/users/:id)
+ * app.delete('/users/:id)
+ */
